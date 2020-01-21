28bd37623385e8b17340fff5c7a73202fb8877e2
rathnak <t160d511@gunma-u.ac.jp>
2020-01-21 17:35:38 +0900

    Improve testing function

--- Predict.py
+++ Predict.py
@@ -22,7 +22,7 @@ class Predict:
         for model in self.ng_models:
             model.pre_process(context, training_size=t_size)
 
-        self.poses2suggest = FreqDist()
+        self._poses2suggest = FreqDist()
 
     def prior_probs(self, sentence, model, word_nums=5):
         sentence = sentence.lower()
@@ -67,34 +67,47 @@ class Predict:
             )
 
         pos_token_freq = dict()
+
         for (word, pos), freq in tmp_data.items():
             if pos[-1] not in pos_token_freq:
                 pos_token_freq[pos[-1]] = FreqDist()
+
             pos_token_freq[pos[-1]].update({word[-1]:freq})
 
         pos_freq_token = dict()
         for pos, token_freq in pos_token_freq.items():
             pos_freq_token[pos] = dict()
+
             for token, freq in token_freq.items():
                 if freq not in pos_freq_token[pos]:
                     pos_freq_token[pos][freq] = []
+
                 pos_freq_token[pos][freq].append(token)
 
         return pos_freq_token
 
-    def pos_ngram_test(self, order=3):
+    def pos_rank(self, arg):
+        mst_common = self._poses2suggest.most_common()
+        for k, (pos, _) in enumerate(mst_common):
+            if arg == pos:
+                return k
+        return len(mst_common)
+
+    def pos_ngram_test(self, sug_nums = 3):
         # >>> conduct testing on bigram + trigram
-        # trigram + brigram
-        # testing's starting time
         start_testing = time.time()
+
+        total_rank = 0
         total = 0
         correct = 0
 
         for sent in self.ng_models[1].test_sents:
+
             sentence = " ".join(sent).lower()
-            pos_tag_ngram = ngrams(pos_tag(word_tokenize(sentence)), order)
+            pos_tag_ngram = ngrams(pos_tag(word_tokenize(sentence)), 3)
 
             for token_poses in pos_tag_ngram:
+                gram_s = time.time()
                 token, pos = tuple(), tuple()
                 for t, p in token_poses:
                     pos += (p,)
@@ -103,36 +116,52 @@ class Predict:
                 # print("{} -> {} : ".format(token, pos))
 
                 self.update_suggestion(pos[:-1])
-                most_common3 = {fst for fst, snd in self.poses2suggest.most_common(3)}
+                most_common3 = {
+                        fst for fst, snd
+                        in self._poses2suggest.most_common(sug_nums)
+                    }
 
-                verbal_sentence = "Ground Truth : {} -> Suggest : {} : "
-                print(verbal_sentence.format(pos[-1], most_common3))
+                curr_pos = pos[-1]
+                verbal_message1 = "Ground Truth : <{}> -> Suggest : {}"
+                print(verbal_message1.format(curr_pos, most_common3))
+
                 # print(most_common3)
                 total += 1
-                if pos[-1] in most_common3:
+                if curr_pos in most_common3:
                     correct += 1
-                print(correct/total, total)
+
+                accuracy = 100 * (correct / total)
+                print("accuracy = {:.2f}%, total = {}".format(accuracy, total))
+
+                curr_rank = self.pos_rank(curr_pos)
+                total_rank += curr_rank
+                ranking_ratio = total_rank / total
+
+                verbal_message2 = "<{}>'s rank = {}, current_ranking_ratio = {}'"
+                print(verbal_message2.format(curr_pos, curr_rank, ranking_ratio))
+
+                gram_e = time.time()
+                print("exec time = {}".format(gram_e - gram_s))
         # end of testing
         print("exec time = {}".format(time.time()-start_testing))
 
     def update_suggestion(self, sent_poses):
         b_model = self.ng_models[1]
-        epsilon = 0.001
 
-        # initialize poses2suggest data
-        self.poses2suggest = FreqDist()
+        # initialize _poses2suggest data
+        self._poses2suggest = FreqDist()
 
         if len(sent_poses) == 1:
-            # bigram model
+            # apply bigram model only
             all_poses_freq_in_b = \
                     b_model.fetch_if('prefix', sent_poses[-1:])
-            d = all_poses_freq_in_b.N() + epsilon
+            d = all_poses_freq_in_b.N()
             for p, freq in all_poses_freq_in_b.items():
                 n = freq
-                self.poses2suggest.update({p[-1]:n/d})
+                self._poses2suggest.update({p[-1]:n/d})
 
         elif len(sent_poses) == 2:
-            # bigram + trigram model
+            # apply bigram & trigram models
             t_model = self.ng_models[2]
             the_pos_suffix = sent_poses[-1:]
 
@@ -141,7 +170,7 @@ class Predict:
             all_poses_freq_in_t = \
                     t_model.fetch_if('prefix', sent_poses)
 
-            db = all_poses_freq_in_b.N() + epsilon
+            db = all_poses_freq_in_b.N()
             for pos_b, freq_b in all_poses_freq_in_b.items():
                 p, pb, pt = 0, 0, 0
 
@@ -155,13 +184,13 @@ class Predict:
 
                 if pos_t:
                     nt = all_poses_freq_in_t[pos_t]
-                    dt = t_model.fetch_if('prefix', pos_t[:-1]).N() + epsilon
+                    dt = t_model.fetch_if('prefix', pos_t[:-1]).N()
                     pt = nt / dt
 
                 # interpolation
                 p = 0.2 * pb + 0.8 * pt
-                if pos_b[-1] not in self.poses2suggest:
-                    self.poses2suggest.update({pos_b[-1] : p})
+                if pos_b[-1] not in self._poses2suggest:
+                    self._poses2suggest.update({pos_b[-1] : p})
 
     def sent2sent_token_pos(self, sentence):
         # PREPROCESS
@@ -191,7 +220,6 @@ class Predict:
         self.update_suggestion(sent_poses)
 
         #########################################################
-
         # TRY IF THE THE LAST TOKEN EXISTS IN BIGRAM
         last_token = tuple(sent_tokens[-1:])
         # print('last token is: ', last_token)
@@ -201,7 +229,6 @@ class Predict:
         print_info = "----------< {} >----------"+\
                      "\nwith estimated probability ~ {:.2f}%"
         print("\nYou may try...")
-
 
         # last_token to pos_token = lt2pt
         lt2pt = self.last_token2pos_token((last_token),b_model)
@@ -213,12 +240,12 @@ class Predict:
 
         final_pos_score = FreqDist()
         if lt2pt:
-            for pos, prob in self.poses2suggest.items():
+            for pos, prob in self._poses2suggest.items():
                 if pos not in lt2pt:
                     res = 0
                 else:
                     res = sum(lt2pt[pos])/total_freqs_lt2pt
-                    final_pos_score.update({pos: 100* prob * res})
+                    final_pos_score.update({pos: 100 * prob * res})
         # final_pos_score.pprint()
 
         result = final_pos_score.most_common(3)
@@ -229,11 +256,12 @@ class Predict:
 
         if len(result) < 3:
             print("len(result) < 3")
-            result = self.poses2suggest.most_common(mc)
+            result = self._poses2suggest.most_common(mc)
 
-            for pos, score in result:
+            for pos, _ in result:
 
-                print(print_info.format(poscode2word(pos), score))
+                prob_res = self._poses2suggest[pos] * 100
+                print(print_info.format(poscode2word(pos), prob_res))
                 words = list(self.ng_models[0].poses2tokens((pos,)))
 
                 words2suggest = random.sample(
@@ -243,13 +271,14 @@ class Predict:
                 words2suggest = [x[0] for x in words2suggest]
                 for word in words2suggest:
                     print(">>> " + word)
+                print(">>> ...")
         else:
             max_word = 3
-            for pos, score in result:
+            for pos, prob in result:
                 suggest_words = lt2pt[pos]
                 sorted_words = sorted(suggest_words)
 
-                print(print_info.format(poscode2word(pos), score))
+                print(print_info.format(poscode2word(pos), prob))
                 final_words = []
                 for index in sorted_words:
                     for w in suggest_words[index][:max_word]:
@@ -261,6 +290,7 @@ class Predict:
 
                 for word in words2suggest:
                     print(">>> " + word)
+                print(">>> ...")
 
         print("<>"*20)
         # end of predicting
